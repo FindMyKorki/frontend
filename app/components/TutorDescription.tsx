eec0046b49e09846be3d2fffa56ccca4587c4a83
Grzegorzz <plgrzes9@gmail.com>
2025-04-28 20:15:12 +0200

    changed pixel values in classNames

--- app/components/TutorDescription.tsx
+++ app/components/TutorDescription.tsx
@@ -2,21 +2,15 @@ import { FC } from 'react';
 import { View, Text } from 'react-native';
 
 type TutorDescriptionProps = {
-  title?: string;
   className?: string;
   description: string;
 };
 
-const TutorDescription: FC<TutorDescriptionProps> = ({
-  title = 'opis',
-  className,
-  description,
-}) => {
+const TutorDescription: FC<TutorDescriptionProps> = ({ className, description }) => {
   return (
     <View className={`items-center ${className}`}>
-      <Text className="text-[12px] text-primary mb-[10px] font-['Inter']">{title}</Text>
-      <View className="bg-background-alt rounded-[7px] py-[10px] px-[14px]">
-        <Text className="text-[14px] text-text-dark font-['Inter']">{description}</Text>
+      <View className="bg-background-alt rounded-lg py-3 px-4">
+        <Text className="text-base text-text-dark font-['Inter']">{description}</Text>
       </View>
     </View>
   );
