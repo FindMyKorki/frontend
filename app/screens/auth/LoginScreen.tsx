99bd35813f16156a62df1958af3813daa4d1d1c3
Oktawian Kausz <play26400@gmail.com>
2025-05-07 20:41:41 +0200

    Add basic components to LoginScreen

--- app/screens/auth/LoginScreen.tsx
+++ app/screens/auth/LoginScreen.tsx
@@ -1,7 +1,24 @@
-import { ScrollView } from 'react-native';
+import { Text, View } from 'react-native';
+import Auth from '../../../assets/auth.svg';
+import AppButton from '../../components/AppButton';
 
 const LoginScreen = () => {
-  return <ScrollView showsVerticalScrollIndicator={false}></ScrollView>;
+  return (
+    <View className={'flex-1'}>
+      <View className="flex-1 items-center justify-center p-def-x">
+        <Auth height={'90%'} />
+      </View>
+      <View className={'flex-1 items-center justify-center p-def-x'}>
+        <Text>Hej!</Text>
+        <AppButton
+          label={'Zaloguj się z Google'}
+          className={'w-full'}
+          icon={'google'}
+          onPress={() => {}}
+        />
+      </View>
+    </View>
+  );
 };
 
 export default LoginScreen;
